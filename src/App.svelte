31bc3646b40ace1a69e6a80610ba440ab01ae239
N-eeraj <neeraj7rajeena@gmail.com>
2025-05-26 01:06:59 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+const startTime = import.meta.env.VITE_START_DATETIME
+const duration = import.meta.env.VITE_DURATION_HOURS
+
+let now = $state(new Date().getTime())
+const startTimestamp = new Date(startTime).getTime()
+const endTimestamp = startTimestamp + duration * 3600000
+
+setInterval(() => {
+  now += 1000
+}, 1000)
+
+const timeToStart = $derived.by(() => {
+  return startTimestamp - now
+})
+const timeToEnd = $derived.by(() => {
+  return endTimestamp - now
+})
+</script>
+
+<main>
+{#if timeToStart > 0}
+	<p>
+    {timeToStart}
+  </p>
+{:else}
+	<p>
+    {timeToEnd}
+  </p>
+{/if}
+</main>
